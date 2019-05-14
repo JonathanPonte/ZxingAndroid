142b2b88bdffe45b531fa047c70aade560760101
JONATHAN DA PONTE <951547298@acad.unifor.br>
2019-05-14 17:51:13 -0300

    Testando Zxing QRCode

---
+++ app/src/main/java/unifor/br/testevuforia/MainActivity.java
@@ -0,0 +1,131 @@
+package unifor.br.testevuforia;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Environment;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button scan_button;
+    private ImageView image;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scan_button = (Button) findViewById(R.id.scanner_button);
+        image = (ImageView) findViewById(R.id.ImageQR);
+        final Activity activity = this;
+
+
+        scan_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                IntentIntegrator integrator = new IntentIntegrator(activity);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("Scan");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+
+
+            }
+        });
+
+
+        Bitmap bitMap = encodeToQrCode("" , 100, 100);
+
+
+        image.setImageBitmap(bitMap);
+
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+
+        if (result != null) {
+            if (result.getContents() == null) {
+
+                Toast.makeText(this, "cancelado", Toast.LENGTH_SHORT).show();
+
+            } else {
+
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_SHORT).show();
+
+            }
+        } else {
+
+            super.onActivityResult(requestCode, resultCode, data);
+
+        }
+
+
+    }
+
+
+    public static Bitmap encodeToQrCode(String text, int width, int height){
+
+        QRCodeWriter writer = new QRCodeWriter();
+        BitMatrix matrix = null;
+        try {
+            matrix = writer.encode(text, BarcodeFormat.QR_CODE, 100, 100);
+        } catch (WriterException ex) {
+            ex.printStackTrace();
+        }
+        Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        for (int x = 0; x < width; x++){
+            for (int y = 0; y < height; y++){
+                bmp.setPixel(x, y, matrix.get(x,y) ? Color.BLACK : Color.WHITE);
+            }
+        }
+
+
+
+        return bmp;
+    }
+
+
+    public static File savebitmap(Bitmap bmp, String name) throws IOException {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 60, bytes);
+        File f = new File(Environment.getExternalStorageDirectory()
+                + File.separator + name + ".png");
+        f.createNewFile();
+        FileOutputStream fo = new FileOutputStream(f);
+        fo.write(bytes.toByteArray());
+        fo.close();
+        return f;
+    }
+
+
+
+}
